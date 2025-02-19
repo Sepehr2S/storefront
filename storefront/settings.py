ae301750eb31265203c83fe1210f9f10f9d0539f
Sepehr2S <sepehrsaeedizadeh@gmail.com>
2025-02-19 13:28:48 +0330

    creating models and importing data

--- storefront/settings.py
+++ storefront/settings.py
@@ -53,6 +53,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+INTERNAL_IPS = [
+    # ...
+    '127.0.0.1',
+    # ...
+]
+
 ROOT_URLCONF = 'storefront.urls'
 
 TEMPLATES = [
@@ -79,8 +85,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '6568986'
     }
 }
 
