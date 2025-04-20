e639d3f28f66b4ad83961c9fd35f8fc2b02e679c
Sepehr2S <sepehrsaeedizadeh@gmail.com>
2025-04-20 20:14:30 +0330

    creating nested router
    creating reviews for products

--- store/serializer.py
+++ store/serializer.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from store.models import Product, Collection
+from store.models import Product, Collection, Review
 from rest_framework import serializers
 
 
@@ -7,7 +7,6 @@ class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
         fields = ['id', 'title', 'products_count']
-
     products_count = serializers.IntegerField(read_only=True)
 
 
@@ -15,11 +14,17 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
-
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
-
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
 
-   
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'name', 'description', 'date']
+
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return Review.objects.create(product_id=product_id, **validated_data)
--- store/views.py
+++ store/views.py
@@ -4,8 +4,8 @@ from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from .models import Collection, Product, OrderItem
-from .serializer import CollectionSerializer, ProductSerializer
+from .models import Collection, Product, OrderItem, Review
+from .serializer import CollectionSerializer, ProductSerializer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -30,3 +30,10 @@ class CollectionViewSet(ModelViewSet):
         return super().destroy(request, *args, **kwargs)
 
 
+class ReviewViewSet(ModelViewSet):
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+    
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
+    serializer_class = ReviewSerializer
