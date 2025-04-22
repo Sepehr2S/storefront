8eb19f5ae3c865e00437d0b3e5f00bfafde45e5d
Sepehr2S <sepehrsaeedizadeh@gmail.com>
2025-04-22 09:09:42 +0330

    creating sorting and searching

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.db.models.aggregates import Count
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -11,8 +12,10 @@ from .filters import ProductFilter
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    search_fields = ['title', 'description']
+    ordering_fields = ['price_with_tax', 'last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}
