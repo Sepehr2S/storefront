baf98ef3c44bf41997a849be9d6c5028879c0a28
Sepehr2S <sepehrsaeedizadeh@gmail.com>
2025-07-27 08:54:35 +0330

    using smpt4dev to learn how to send email

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,9 @@
+from django.core.mail import BadHeaderError, mail_admins, send_mail
 from django.shortcuts import render
 
-
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    try:
+        send_mail('subject', 'message', 'info@sep.com', ['jesus@sep.com','namosan@sep.com'])
+    except BadHeaderError:
+        pass
+    return render(request, 'hello.html', {'name': 'Sepehr'})
